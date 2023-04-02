34dff0f04437d243559122919a00b60fac5ecb08
Valentin Kim <valentink@yandex.ru>
2023-04-02 20:01:32 +0600

    Задача 1 реализована

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// task_1
+
+const makeElement = (string) =>{
+    return console.log(document.getElementById(string).innerText)
+}
+
+makeElement('string-3')
+makeElement('string-5')
+makeElement('string-2')
+makeElement('string-6')
+makeElement('string-4')
+makeElement('string-1')
